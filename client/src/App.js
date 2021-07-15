b2bdd91aac7d1cf8283efff763845b67e43827a1
Ziva <birulangit1996@gmail.com>
2021-07-15 11:21:47 +0700

    ss

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Join from './pages/Join';
+import Chat from './pages/Chat';
+
+const App = () => {
+  return (
+    <Router>
+      <Route exact path="/" component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
+};
+
+export default App;
---
+++ client/src/pages/Join.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Join = () => {
+  return (
+    <div>
+      <h>Join</h>
+    </div>
+  );
+};
+
+export default Join;
